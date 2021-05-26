6afd2773c2c1de16807f0eb4c5e097463963b7ef
mchayan <dev.roy.manoj@gmail.com>
2021-05-26 13:59:09 +0600

    Maximum & Minimum Value of Java Primitive Types (Int, Short, Long, Byte, Float, Double)

--- MaxMinValues/src/manoj/prayan/Main.java
+++ MaxMinValues/src/manoj/prayan/Main.java
@@ -5,30 +5,36 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int intSampleNumber = 1;
+
         int MaxIntValue = Integer.MAX_VALUE;
         int MinIntValue = Integer.MIN_VALUE;
         System.out.println("Integer Maximum Value is : " +MaxIntValue);
         System.out.println("Integer Minimum Value is : " +MinIntValue);
 
-        byte byteSampleNumber = 1;
         byte MaxByteValue = Byte.MAX_VALUE;
         byte MinByteValue = Byte.MIN_VALUE;
         System.out.println("Byte Maximum Value is : " +MaxByteValue);
         System.out.println("Byte Minimum Value is : " +MinByteValue);
 
-        short shortSampleNumber = 1;
         short MaxShortValue = Short.MAX_VALUE;
         short MinShortValue = Short.MIN_VALUE;
         System.out.println("Short Maximum Value is : " +MaxShortValue);
         System.out.println("Short Minimum Value is : " +MinShortValue);
 
-
-        long longSampleNumber = 1;
         long MaxLongValue = Long.MAX_VALUE;
         long MinLongValue = Long.MIN_VALUE;
         System.out.println("Long Maximum Value is : " +MaxLongValue);
         System.out.println("Long Minimum Value is : " +MinLongValue);
+
+        float MaxFloatValue = Float.MAX_VALUE;
+        float MinFloatValue = Float.MIN_VALUE;
+        System.out.println("Float Maximum Value is : " +MaxFloatValue);
+        System.out.println("Float Minimum Value is : " +MinFloatValue);
+
+        double MaxDoubleValue = Double.MAX_VALUE;
+        double MinDoubleValue = Double.MIN_VALUE;
+        System.out.println("Double Maximum Value is : " +MaxDoubleValue);
+        System.out.println("Double Minimum Value is : " +MinDoubleValue);
 
     }
 }
