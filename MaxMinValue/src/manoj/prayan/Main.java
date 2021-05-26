fadc166700a1d2c0a8f68af858d9c0d2c24f705f
mchayan <dev.roy.manoj@gmail.com>
2021-05-26 12:37:43 +0600

    Maximum & Minimum Value of Some Primitive Data Types In Java (Int, Long, Short & Byte)

---
+++ MaxMinValue/src/manoj/prayan/Main.java
@@ -0,0 +1,34 @@
+package manoj.prayan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+	int intSampleNumber = 1;
+	int MaxIntValue = Integer.MAX_VALUE;
+	int MinIntValue = Integer.MIN_VALUE;
+	System.out.println("Integer Maximum Value is : " +MaxIntValue);
+	System.out.println("Integer Minimum Value is : " +MinIntValue);
+
+	byte byteSampleNumber = 1;
+	byte MaxByteValue = Byte.MAX_VALUE;
+	byte MinByteValue = Byte.MIN_VALUE;
+	System.out.println("Byte Maximum Value is : " +MaxByteValue);
+	System.out.println("Byte Minimum Value is : " +MinByteValue);
+
+	short shortSampleNumber = 1;
+	short MaxShortValue = Short.MAX_VALUE;
+	short MinShortValue = Short.MIN_VALUE;
+	System.out.println("Short Maximum Value is : " +MaxShortValue);
+	System.out.println("Short Minimum Value is : " +MinShortValue);
+
+
+	long longSampleNumber = 1;
+	long MaxLongValue = Long.MAX_VALUE;
+	long MinLongValue = Long.MIN_VALUE;
+	System.out.println("Long Maximum Value is : " +MaxLongValue);
+	System.out.println("Long Minimum Value is : " +MinLongValue);
+
+    }
+}
