cd2f1cdce104a7ad22688a8bc975a822a8bf804c
mchayan <dev.roy.manoj@gmail.com>
2021-05-26 14:32:05 +0600

    Char, Boolean & Strings Data Types Example

---
+++ CharAndBoolean/src/prayan/manoj/Main.java
@@ -0,0 +1,14 @@
+package prayan.manoj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char FirstCharacter = 'I';
+	boolean MyBoolean = true;
+	String Connect = "ly";
+	char SecondCharacter = '\u2764';
+	String LastString = "Programming";
+	System.out.println(""+FirstCharacter + " "+MyBoolean+ ""+Connect + " "+SecondCharacter+ " "+LastString+".");
+
+    }
+}
